0101024ecb0549db691d146895601168f1d81458
Tara Dia <taracdia@gmail.com>
2021-04-13 18:13:47 -0500

    Added card flipping animation

--- src/components/TarotCard.js
+++ src/components/TarotCard.js
@@ -1,27 +1,61 @@
-import { useState } from "react";
-import { Col } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { Button, Col } from "react-bootstrap";
+import ReactCardFlip from "react-card-flip";
+
 function TarotCard({ card }) {
 	const [displayImage, setDisplayImage] = useState(true);
+	const [isFlipped, setIsFlipped] = useState(false);
 	const handleClick = () => {
 		setDisplayImage(!displayImage);
 	};
 
+	useEffect(() => {
+		setIsFlipped(true);
+	}, []);
+
+	const cardWithFlip = (
+		<ReactCardFlip
+			isFlipped={isFlipped}
+			flipSpeedFrontToBack={5}
+			flipSpeedBackToFront={5}
+		>
+			<div>
+				<img
+					src="/cardImages/back.jpg"
+					height={525}
+					width={300}
+					alt="back of a card"
+				/>
+				{/* Button has to be present for flipping to work */}
+				<Button className="hide" onClick={() => setIsFlipped(true)}>
+					Click
+				</Button>
+			</div>
+			<div>
+				<img
+					src={`/cardImages/${card.name_short}.jpg`}
+					alt={card.desc}
+					height={525}
+					width={300}
+					className={
+						card.reverse ? "reversed clickable" : "clickable"
+					}
+					onClick={handleClick}
+				/>
+			</div>
+		</ReactCardFlip>
+	);
+
 	return (
 		<Col className={TarotCard}>
 			<h2>{card.name}</h2>
-			<div className="clickable" onClick={handleClick}>
-				{displayImage ? (
-					<img
-						src={`/cardImages/${card.name_short}.jpg`}
-						alt={card.desc}
-						height={525}
-						width={300}
-						className={card.reverse ? "reversed" : ""}
-					/>
-				) : (
-					<p>{card.desc}</p>
-				)}
-			</div>
+			{displayImage ? (
+				cardWithFlip
+			) : (
+				<p className="clickable" onClick={handleClick}>
+					{card.desc}
+				</p>
+			)}
 			{card.reverse ? (
 				<p>{card.meaning_rev}</p>
 			) : (
